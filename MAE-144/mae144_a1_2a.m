9e078540ce623273fef833b8198aa82212567221
AJMarsh-Eng <146989549+AJMarsh-Eng@users.noreply.github.com>
2023-10-12 04:00:29 -0700

    update

--- MAE-144/mae144_a1_2a.m
+++ MAE-144/mae144_a1_2a.m
@@ -1,3 +1,5 @@
-%teoiug
+%Code for problem 2a of A1. 
+%goal is to make D(s) controller [y(s)/x(s)] to place poles of tf at s={-1,-1,-3,-3,-6,-6} 
+clear,clc
 b=RR_poly([-2 2 -5 5],1), a=RR_poly([-1 1 -3 3 -6 6],1), f1=RR_poly([-1 -1 -3 -3 -6 -6],1)
 [x1,y1] = RR_diophantine(a,b,f1), test1=trim(a*x1+b*y1), residual1=norm(f1-test1)
---
+++ MAE-144/mae144_a1_2b.m
@@ -0,0 +1,5 @@
+%Code for problem 2b of A1. 
+%goal is to make the D(s) controller [y(s)/x(s)] from 2a rational
+clear,clc
+b=RR_poly([-2 2 -5 5],1), a=RR_poly([-1 1 -3 3 -6 6],1), f1=RR_poly([-1 -1 -3 -3 -6 -6 -20 -20 -20 -20 -20 -20],1)
+[x1,y1] = RR_diophantine(a,b,f1), test1=trim(a*x1+b*y1), residual1=norm(f1-test1)
